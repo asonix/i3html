d883c63cde994687230416a63190123e6c49610b
Asonix <asonix.dev@gmail.com>
2014-04-30 23:10:16 -0500

    fixed contact page spacing

--- js/contact.js
+++ js/contact.js
@@ -10,11 +10,12 @@ function Contact() {
         this.create1();
     }
     this.create1 = function() {
-        q.html.html("<div class=\"contact\"></div>");
-        q.html = html.find(".contact");
+        q.html.html("<div class=\"settings\"></div>");
+        q.html = html.find(".settings");
 
         q.html.append("<div class=\"headerbar\"></div>");
         q.header = q.html.find(".headerbar");
+        q.header.append("<span class=\"hleft\"><div style=\"width: 24px; height: 24px;\"></div></span>");
         q.header.append("<span class=\"hcenter\">Contact</span>");
         q.hcenter = q.header.find(".hcenter");
 
@@ -22,7 +23,8 @@ function Contact() {
         q.content = q.html.find(".content");
         q.content.append("<div class=\"contentScrolling\"></div>");
         q.scrolling = q.content.find(".contentScrolling");
-        q.scrolling.html("<form class=\"contactus\" action=\"php/email.php\" method=\"post\" enctype=\"text/plain\"></form>");
+        //q.scrolling.html("<form class=\"contactus\" action=\"php/email.php\" method=\"post\" enctype=\"text/plain\"></form>");
+        q.scrolling.html("<form class=\"contactus\" action=\"MAILTO:asonix.dev@gmail.com\" method=\"post\" enctype=\"text/plain\"></form>");
         q.form = q.scrolling.find(".contactus");
         q.form.append("<table></table>");
         q.form.find("table").append("<tr><td>Name:</td><td><input type=\"text\" name=\"name\" /></td></tr>");
@@ -30,7 +32,8 @@ function Contact() {
         q.form.find("table").append("<tr><td colspan=\"2\">Message:</td></tr>");
         q.form.find("table").append("<tr><td colspan=\"2\"><textarea name=\"message\"></textarea></td></tr>");
         q.form.find("table").append("<tr><td colspan=\"2\"><input type=\"submit\" value=\"Submit\" /></td></tr>");
-        validateForm();
+        size(q.html,q.content,q.header);
+        //validateForm();
     }
 }
 
